332463f70d469cdd7b175f013fa96aa2848e054a
AvoeV <Salena1992@mail.ru>
2025-09-15 16:49:34 +0300

    ДЗ Управление потоком

---
+++ 5-languages/app.js
@@ -0,0 +1,24 @@
+
+let lang = 'es';
+
+switch (lang) {
+    case 'en':
+        console.log('Hello!');
+        break
+    case 'ru':
+        console.log('Здравствуйте!');
+        break
+    case 'de':
+        console.log('Hallo!');
+        break
+    case 'fr':
+        console.log('Bonjour!');
+        break
+    case 'es':
+        console.log('¡Hola!');
+        break
+    default:
+        console.log('Язык не поддерживается!');
+}
+
+

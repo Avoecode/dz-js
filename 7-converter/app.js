b8ef6296a85f88f49cec0bb43f85ad118a2edb36
AvoeV <Salena1992@mail.ru>
2025-10-01 16:57:25 +0300

    Домашнее задание исправленное Функции

--- 7-converter/app.js
+++ 7-converter/app.js
@@ -1,24 +1,42 @@
 //Курсы
 const EXC_USD_RUB = 82.8;//доллар к рублю
 const EXC_EUR_RUB = 97.6;//евро к рублю
-const EXC_THB_RUB = 2.57;//тайский бат к рублю
+const EXC_THB_RUB = 2.5;//тайский бат к рублю
+const EXC_EUR_THB = 38.03;//евро к бату
+const EXC_USD_EUR = 1.18;//доллар к евро
+const EXC_USD_THB = 32.35;//доллар к бату
+
 
 function calculateEx(amountOfMoney, originalCurrency, targetCurrency) {
     if (originalCurrency === 'RUB' && targetCurrency === 'USD') {
-        return amountOfMoney / EXC_USD_RUB;//пересчет из рублей в доллары
+        return amountOfMoney / EXC_USD_RUB;//рубли в доллары
     } else if (originalCurrency === 'USD' && targetCurrency === 'RUB') {
-        return amountOfMoney * EXC_USD_RUB;//пересчет из доллары в рубли
+        return amountOfMoney * EXC_USD_RUB;//доллары в рубли
+    } else if (originalCurrency === 'USD' && targetCurrency === 'THB') {
+        return amountOfMoney * EXC_USD_THB;//доллары в баты
+    } else if (originalCurrency === 'THB' && targetCurrency === 'USD') {
+        return amountOfMoney / EXC_USD_THB;//баты в доллары 
+    } else if (originalCurrency === 'USD' && targetCurrency === 'EUR') {
+        return amountOfMoney / EXC_USD_EUR;//доллары в евро
+    } else if (originalCurrency === 'EUR' && targetCurrency === 'USD') {
+        return amountOfMoney * EXC_USD_EUR;//евро в доллары 
     } else if (originalCurrency === 'RUB' && targetCurrency === 'EUR') {
-        return amountOfMoney / EXC_EUR_RUB;//пересчет из рублей в евро
+        return amountOfMoney / EXC_EUR_RUB;//рубли в евро
     } else if (originalCurrency === 'EUR' && targetCurrency === 'RUB') {
-        return amountOfMoney * EXC_EUR_RUB;//пересчет из евро в рубли
+        return amountOfMoney * EXC_EUR_RUB;//евро в рубли
+    } else if (originalCurrency === 'THB' && targetCurrency === 'EUR') {
+        return amountOfMoney / EXC_EUR_THB;//баты в евро
+    } else if (originalCurrency === 'EUR' && targetCurrency === 'THB') {
+        return amountOfMoney * EXC_EUR_THB;//евро в баты
     } else if (originalCurrency === 'RUB' && targetCurrency === 'THB') {
-        return amountOfMoney / EXC_THB_RUB;//пересчет из рублей в баты
+        return amountOfMoney / EXC_THB_RUB;//рубли в баты
     } else if (originalCurrency === 'THB' && targetCurrency === 'RUB') {
-        return amountOfMoney * EXC_THB_RUB;//пересчет из бат в рубли  
+        return amountOfMoney * EXC_THB_RUB;//баты в рубли  
     } else {
         return null; // для неподдерживаемых валют
     }
 }
     
-console.log(calculateEx(10000, 'EUR', 'RUB'));
+console.log(calculateEx(2000, 'RUB', 'EUR'));
+console.log(calculateEx(5500, 'THB', 'USD'));
+

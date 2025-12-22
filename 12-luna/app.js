c07df29752f326bd5c3d09a743d7008d8f15ca94
AvoeV <Salena1992@mail.ru>
2025-12-22 18:50:15 +0300

    ДЗ Работа со строками исправленное

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -14,16 +14,19 @@ function luna(str) {
         }
     }
     // Преобразуем строку в массив чисел
-    const numbers = cleaned.split('').map(Number);  
+    const numbers = cleaned.split('').map(Number);
+     // Обработка по алгоритму Луна
     const filterNumber = numbers.map((num, i) => {
-        if (i % 2 === 0) {
+        if ((numbers.length - 1 - i) % 2 === 1) { //проверяет, является ли позиция цифры с конца нечетной (вторая, четвертая и т.д. с конца)
             const doubled = num * 2;
             return doubled > 9 ? doubled - 9 : doubled;
         }
         return num;
     });
+    // Суммируем все цифры
     const sumOfNumber = filterNumber.reduce((sum, current) => sum + current, 0);
     
+    // Проверяем, делится ли сумма на 10 без остатка
     if (sumOfNumber % 10 === 0) {
         return true;
     } else {
